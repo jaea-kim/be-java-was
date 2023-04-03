e578a3641bd9e518fb39a3ad3d838db976187f33
jaea-kim <jaeakim98@gmail.com>
2023-04-03 16:48:57 +0900

    path에 해당하는 파일 읽어 body에 저장

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,9 +2,11 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpParser;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -21,13 +23,9 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            String httpHeader;
-            while ((httpHeader = br.readLine()) != null) {
-                System.out.println(httpHeader);
-            }
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            String path = HttpParser.getPathInRequestLine(br.readLine());
+            byte[] body = Files.readAllBytes(new File("src/main/resources/templates" + path).toPath());
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
