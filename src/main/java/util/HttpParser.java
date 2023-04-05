1cbe59185185a2f05289a27559c09568c72462c6
jaea-kim <jaeakim98@gmail.com>
2023-04-05 12:28:41 +0900

    queryString 받으서 파싱해주는 기능 구현

--- src/main/java/util/HttpParser.java
+++ src/main/java/util/HttpParser.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HttpParser {
     public static String getMethodInRequestLine(String requestLine) {
         return requestLine.split(" ")[0];
@@ -10,10 +13,22 @@ public class HttpParser {
     }
 
     public static String getAbsolutePath(String requestTarget) {
-        return requestTarget.split("//?")[0];
+        return requestTarget.split("[?]")[0];
     }
 
-    public static String getQuery(String requestTarget) {
-        return requestTarget.split("//?")[1];
+    public static String getQueryString(String requestTarget) {
+        return requestTarget.split("[?]")[1];
+    }
+
+    public static Map<String, String> getParam(String queryString) {
+        Map<String, String> params = new HashMap<>();
+        String[] querys = queryString.split("&");
+
+        for (String query : querys) {
+            String[] q = query.split("=");
+            params.put(q[0], q[1]);
+        }
+
+        return params;
     }
 }
