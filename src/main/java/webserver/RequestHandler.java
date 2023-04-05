40fc703686a5b7073de0b8d04562f1d34114ddbe
jaea-kim <jaeakim98@gmail.com>
2023-04-05 10:22:53 +0900

    run 메서드 개선

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpParser;
 
+import javax.sql.DataSource;
+
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
@@ -22,34 +24,34 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            String path = HttpParser.getPathInRequestLine(br.readLine());
-            byte[] body = Files.readAllBytes(new File("src/main/resources/templates" + path).toPath());
+            //입력으로 들어온 내용 전부 request로 보냄
+            Request request = new Request(in);
+            //받은 request를 가지고 requestMapping을 해주는 requestController 생성
+            RequestController requestController = new RequestController(request);
             DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+
+            //requestcontroller에서 다 처리 후 결론적으로 보여줘야할 뷰 경로를 알려주는게 맞지 않을까,, + 상태코드까지?
+            if (requestController.isMappingView()) {
+                logger.debug("in static view");
+                Response response = new Response("200", request.getPath());
+                sendResponse(dos, response);
+
+            } else {
+                requestController.mapping();
+            }
+
+
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
+    private void sendResponse(DataOutputStream dos, Response response) throws IOException {
+        byte[] body = response.getResponseBody();
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+        dos.writeBytes(response.getResponseHeader());
+        dos.writeBytes("\r\n");
+        dos.write(body, 0, body.length);
+        dos.flush();
     }
 }
