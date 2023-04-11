d44e5b50b598b05bc0594002382d4f72755b72b8
jaea-kim <jaeakim98@gmail.com>
2023-04-11 16:29:25 +0900

    response 생성자 변경

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -22,36 +22,32 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             //입력으로 들어온 내용을 매개변수로 받아 request 객체 생성
             Request request = new Request(in);
-            DataOutputStream dos = new DataOutputStream(out);
             Response response;
+            DataOutputStream dos = new DataOutputStream(out);
             RequestController requestController = RequestController.getRequestController();
 
             //정적 데이터 조회
             if (requestController.isMappingView(request)) {
-                logger.debug("request static data");
-                logger.debug("request accept : {}", request.getAccept());
-                response = new Response(200, request.getPath());
+                logger.debug("request static data : {}", request.getPath());
+                response = new Response(200, request.getPath(), request.getAccept());
             } else {
                 //동적 데이터 조회 및 데이터 전송 상황
+                logger.debug("request data : {}", request.getPath());
                 String url = requestController.getMapping(request);
                 logger.debug("new mapping url : {}", url);
-                logger.debug("request accept : {}", request.getAccept());
-                response = new Response(200, url);
+                response = new Response(302, url, request.getAccept());
             }
 
             sendResponse(dos, response);
-
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
     private void sendResponse(DataOutputStream dos, Response response) throws IOException {
-        byte[] body = response.getResponseBody();
-
-        dos.writeBytes(response.getResponseHeader());
+        dos.writeBytes(response.getMessageHeader());
         dos.writeBytes("\r\n");
-        dos.write(body, 0, body.length);
+        dos.write(response.getMessageBody(), 0, response.getMessageBody().length);
         dos.flush();
     }
 }
