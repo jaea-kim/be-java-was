2aeb8a7cda1f56138f6f58b8731f9463825c1aa9
jaea-kim <jaeakim98@gmail.com>
2023-04-18 19:00:32 +0900

    findUserById 반환을 optional로 감싸도록 변경

--- src/main/java/user/UserController.java
+++ src/main/java/user/UserController.java
@@ -1,9 +1,11 @@
 package user;
 
+import db.Database;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
@@ -21,8 +23,18 @@ public class UserController {
     }
     public String join(Map<String, String> params) {
         User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+        Database.addUser(user);
         logger.debug("user : {}" , user.toString());
 
         return "redirect:/index.html";
+    }
+
+    public String login(Map<String, String> params) {
+        Optional<User> user = Database.findUserById(params.get("userId"));
+        if (user.isPresent()) {
+            //Todo: http 헤더의 쿠키 값 설정, 여기서 하는게 맞을까?
+            return "redirect:/index.html";
+        }
+        return "/user/login_failed.html";
     }
 }
